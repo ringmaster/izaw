ca5b54f95b198b0579c32b6833950e0e7cfd79ab
ringmaster <a_github@midnightcircus.com>
2012-06-21 01:26:57 -0400

    Add user template.

---
+++ post.php
@@ -0,0 +1,40 @@
+<article id="post-<?php echo $content->id; ?>" class="post" itemscope itemtype="http://schema.org/BlogPosting">
+
+	<header class="metadata">
+		<h1 itemprop="name"><a href="<?php echo $content->permalink; ?>" itemprop="url"><?php echo $content->title_out; ?></a></h1>
+		<div class="pubdata">
+			<span itemprop="author" itemscope itemtype="http://schema.org/Person"><span itemprop="name"><?php echo $content->author->username; ?></span></span>
+			<time datetime="<?php echo $content->pubdate->format('Y-m-d\TH:i:s\Z'); ?>" itemprop="datePublished"><?php echo $content->pubdate->format(Options::get('dateformat') . ' ' . Options::get('timeformat')); ?></time>
+		</div>
+		<div itemprop="keywords" class="tags">
+			<?php echo Format::tag_and_list($content->tags, ', ', ', and '); ?>
+		</div>
+	</header>
+
+	<?php echo $theme->content($content->author); ?>
+
+	<div class="content" itemprop="articleBody">
+		<?php echo $content->excerpt; ?>
+	</div>
+
+	<?php if($request->display_entry): ?>
+
+	<section class="comments" itemprop="comment">
+		<h1 id="comments">Comments</h1>
+		<?php if($content->comments->moderated->count == 0): ?>
+			<p><?php _e('There are no comments on this post.'); ?>
+		<?php else: ?>
+			<?php foreach($content->comments->moderated->comments as $comment): ?>
+				<?php echo $theme->content($comment); ?>
+			<?php endforeach; ?>
+		<?php endif; ?>
+		<?php if($post->info->comments_disabled): ?>
+			<p><?php _e('Sorry, commenting on this post is disabled.'); ?>
+		<?php else: ?>
+		<?php $post->comment_form()->out(); ?>
+		<?php endif; ?>
+	</section>
+
+	<?php endif; ?>
+
+</article>
---
+++ user.php
@@ -0,0 +1,2 @@
+This is the user template.
+<?php Utils::debug($content); ?>
